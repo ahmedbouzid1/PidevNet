0bf42a07d160fb375d32f557af659e12db81a529
walid <walid@LAPTOP-Q9O1RHHC>
2019-12-02 14:49:51 +0100

    dfgd

---
+++ Pidev.Presentation/Controllers/PsuhController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pidev.Presentation.Controllers
+{
+    public class PsuhController : Controller
+    {
+        // GET: Psuh
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
